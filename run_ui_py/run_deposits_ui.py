b0e3a8f9ff97da349999f554e342d0e49d0e5eb7
Younes <yoneszamiri6@gmail.com>
2023-05-26 20:02:13 +0330

    update program

--- run_ui_py/run_deposits_ui.py
+++ run_ui_py/run_deposits_ui.py
@@ -16,7 +16,10 @@ class MyAddDeposits(DepositsUI):
         code = self.lineEdit_NationalCode.text()
         if code == "":
             condition = False
-        isbn = self.lineEdit_ISBN.text()
+        else:
+            code = int(self.lineEdit_NationalCode.text())
+
+        isbn = int(self.lineEdit_ISBN.text())
         if isbn == "":
             condition = False
         DateDeliveryFromPerson = self.lineEdit_DateDeliveryFromPerson.text()
@@ -27,24 +30,51 @@ class MyAddDeposits(DepositsUI):
             condition = False
 
         if condition:
+            condition = False
             conn = sqlite3.connect("./DataBase/libDatabase.db")
+            # find national code
             c = conn.cursor()
-            # c.execute(f"SELECT * FROM Person WHERE code = {code}")
-            # records = c.fetchall()
-            # for rec in records:
-            #     print(rec)
-            c.executescript(f"""INSERT INTO deposits(code,ISBN,DateDeliveryToPerson,DateDeliveryFromPerson)VALUES({code},{isbn},'{DateDeliveryToPerson}','{DateDeliveryToPerson}')""")
-            conn.commit()
-            conn.close()
+            c.execute(f"SELECT * FROM Person")
+            records = c.fetchall()
+            for rec in records:
+                if rec[2] == code:
+                    condition = True
+                else:
+                    condition = False
+                    self.label_Alert.setText("isbn or national code are not valid")
+            # find isbn code
 
-            self.lineEdit_NationalCode.clear()
-            self.lineEdit_ISBN.clear()
-            self.lineEdit_DateDeliveryToPerson.clear()
-            self.lineEdit_DateDeliveryFromPerson.clear()
+            c.execute(f"SELECT * FROM book")
+            records = c.fetchall()
+            for rec in records:
+                if rec[2] == isbn:
+                    condition = True
+                else:
+                    condition = False
+                    self.label_Alert.setText("isbn or national code are not valid")
 
-            self.label_Alert.setStyleSheet("color:rgb(40, 77, 0);\n"
-                                           "margin-left:3px")
-            self.label_Alert.setText("data saved")
+            # search for book and code in deposits
+            c.execute(f"SELECT * FROM deposits")
+            records = c.fetchall()
+            for rec in records:
+                if rec[0] == code and rec[1] == isbn:
+                    condition = False
+                    self.label_Alert.setText("This book has already been loaned to this person")
+                else:
+                    condition = True
+
+            if condition:
+                c.executescript(f"""INSERT INTO deposits(code,ISBN,DateDeliveryToPerson,DateDeliveryFromPerson)VALUES({code},{isbn},'{DateDeliveryToPerson}','{DateDeliveryToPerson}')""")
+                conn.commit()
+                conn.close()
+                self.lineEdit_NationalCode.clear()
+                self.lineEdit_ISBN.clear()
+                self.lineEdit_DateDeliveryToPerson.clear()
+                self.lineEdit_DateDeliveryFromPerson.clear()
+
+                self.label_Alert.setStyleSheet("color:rgb(40, 77, 0);\n"
+                                               "margin-left:3px")
+                self.label_Alert.setText("data saved")
 
         else:
             self.label_Alert.setText("Please complete the form")
